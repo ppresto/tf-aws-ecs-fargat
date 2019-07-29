2e1a03dc3ed54a514ea43ff3397954cd63463f47
ppresto <ppresto@hashicorp.com>
2019-07-29 14:52:53 -0700

    reverting back...

--- ecs.tf
+++ ecs.tf
@@ -5,17 +5,17 @@ resource "aws_ecs_cluster" "main" {
 }
 
 data "template_file" "cb_app" {
-  #template = file("./templates/ecs/cb_app.json.tpl")
-  #vars = {
-  #  app_name       = var.name_prefix
-  #  app_image      = var.app_image
-  #  app_port       = var.app_port
-  #  fargate_cpu    = var.fargate_cpu
-  #  fargate_memory = var.fargate_memory
-  #  aws_region     = var.aws_region
-  #}
+  template = file("./templates/ecs/cb_app.json.tpl")
+  vars = {
+    app_name       = var.name_prefix
+    app_image      = var.app_image
+    app_port       = var.app_port
+    fargate_cpu    = var.fargate_cpu
+    fargate_memory = var.fargate_memory
+    aws_region     = var.aws_region
+  }
 
-  templatefile("${path.module}/templates/ecs/cb_app.json.tpl", {app_name = var.name_prefix, app_image = var.app_image, app_port = var.app_port, fargate_cpu = var.fargate_cpu, fargate_memory = var.fargate_memory, aws_region = var.aws_region })
+  #templatefile("${path.module}/templates/ecs/cb_app.json.tpl", {app_name = var.name_prefix, app_image = var.app_image, app_port = var.app_port, fargate_cpu = var.fargate_cpu, fargate_memory = var.fargate_memory, aws_region = var.aws_region })
 }
 
 resource "aws_ecs_task_definition" "app" {
