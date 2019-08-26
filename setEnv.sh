36a66375e47690eaa340f33e7719ae64e3073536
ppresto <ppresto@hashicorp.com>
2019-08-26 15:46:08 -0700

    exporting variable

--- setEnv.sh
+++ setEnv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set Region in tf alias
-PROJECT="ppresto-ecs"
+export PROJECT="ppresto-ecs"
 alias tf='DATE=$(date +%m%d%Y-%H%M); terraform plan -var "name_prefix=${PROJECT}" -out ./.terraform/tfplan.$DATE && terraform apply ./.terraform/tfplan.$DATE'
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias tfd='terraform destroy -var "name_prefix=${PROJECT}" -auto-approve'
